fc66329754dfb93feb3a59fc5a5ff70daf87d82e
YukiNGSM <ykh2135239@stu.o-hara.ac.jp>
2022-11-10 12:38:01 +0900

    認証機能大規模改変前

--- meplus/settings.py
+++ meplus/settings.py
@@ -30,7 +30,6 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'django_bootstrap5',
-
 ]
 
 MIDDLEWARE = [
@@ -192,8 +191,8 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_EMAIL_REQUIRED = True
 
 # # ログイン/ログアウト後の遷移先を設定
-# LOGIN_REDIRECT_URL = 'match:chat_list'
-# ACCOUNT_LOGOUT_REDIRECT_URL = 'account_login'
+LOGIN_REDIRECT_URL = 'match:home'
+ACCOUNT_LOGOUT_REDIRECT_URL = 'account_login'
 
 # ログアウトリンク一発でログアウトする設定
 ACCOUNT_LOGOUT_ON_GET = True
